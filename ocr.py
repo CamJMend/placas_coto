7eb46fe5d89ed3fef6a6fe3be6f65dd62e98a24e
CamJMend <cam.jmend@gmail.com>
2025-02-10 08:29:08 -0600

    New data

--- ocr.py
+++ ocr.py
@@ -7,7 +7,7 @@ pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
 def extract_text(plate_image):
     image = imutils.resize(plate_image, width=1000)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    #blurred = cv2.GaussianBlur(gray, (2, 2), 0)
     #cv2.imshow("Blurred", blurred)
     #binary = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     #cv2.imshow("Binary", binary)
